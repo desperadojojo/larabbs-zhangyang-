59a99e6480ca9ba70450b68a8ddcfa478ac9b19a
desperadojojo <25340029@qq.com>
2018-12-03 08:32:58 +0000

    List topics created by user

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,8 +8,12 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
-    {
+	/**
+	 * @param Category $category
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
+	public function show(Category $category)
+	{
     	//读取分类ID关联的话题，并按每20条分页
 	    $topics = Topic::where('category_id',$category->id)->paginate(20);
 	    //传参变量话题和分类到模板中
