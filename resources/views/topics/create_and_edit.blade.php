bdb13d17cde23b8891bd6b0819ceea3f1c229d3e
desperadojojo <25340029@qq.com>
2018-12-06 08:43:17 +0000

    Prevent XSS injection

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -57,7 +57,7 @@
 
 @endsection
 
-@section('styles')
+{{--@section('styles')
     <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
 @stop
 
@@ -68,19 +68,19 @@
     <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
 
     <script>
-    $(document).ready(function(){
-        var editor = new Simditor({
-            textarea: $('#editor'),
-            upload: {
-                url: '{{ route('topics.upload_image') }}',
-                params: { _token: '{{ csrf_token() }}' },
-                fileKey: 'upload_file',
-                connectionCount: 3,
-                leaveConfirm: '文件上传中，关闭此页面将取消上传。'
-            },
-            pasteImage: true,
+        $(document).ready(function(){
+            var editor = new Simditor({
+                textarea: $('#editor'),
+                upload: {
+                    url: '{{ route('topics.upload_image') }}',
+                    params: { _token: '{{ csrf_token() }}' },
+                    fileKey: 'upload_file',
+                    connectionCount: 3,
+                    leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+                },
+                pasteImage: true,
+            });
         });
-    });
     </script>
 
-@stop
+@stop--}}
