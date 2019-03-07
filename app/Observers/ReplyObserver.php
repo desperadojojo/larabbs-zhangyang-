80690048fd6fb3c63e2dfc216f03388b4d691337
desperadojojo <25340029@qq.com>
2019-03-07 02:20:52 +0000

    notifications stats

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -15,8 +15,10 @@ class ReplyObserver
         $topic = $reply->topic;
         $topic->increment('reply_count',1);
 
-        //通知作者话题被回复了
-        $topic->user->notify(new TopicReplied($reply));//实例化 TopicReplied 通知类
+        //通知作者话题被回复了 如果评论的作者不是话题的作者，才需要通知
+        if ( ! $reply->user->isAuthorOf($topic)) {
+            $topic->user->notify(new TopicReplied($reply));
+        }//实例化 TopicReplied 通知类
     }
 
     public function creating(Reply $reply)
